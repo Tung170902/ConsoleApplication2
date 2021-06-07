20c8113aba3c2d1a3fff22e4f9a78ee10e85e78a
Tung170902 <70988911+Tung170902@users.noreply.github.com>
2021-06-07 11:25:36 +0700

    Initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApplication2.Program
+{
+    public class Cylinder
+    {
+        public double Radius;
+        public double Height;
+        public double BaseArea;
+        public double LateralArea;
+        public double TotalArea;
+        public double Volume;
+
+        
+        
+        public void Process()
+        {
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Math.PI * Radius * Height;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("Base: " + BaseArea + "; " + "Lateral: " + LateralArea + "; " + "Total: " + TotalArea + "; " + "Volume: " + Volume);
+        }
+
+        public static void Main(string[] args)
+        {
+            Cylinder cl = new Cylinder();
+            Console.WriteLine("Radius: ");
+            cl.Radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            cl.Height = Convert.ToDouble(Console.ReadLine());
+            cl.Process();
+            cl.Result();
+        }
+    }
+}
